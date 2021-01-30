143fddb7e7028ae3b063adecc66b73fa3bd4464d
loganmcmullen <logan.mcmullen@live.com>
2021-01-30 15:03:15 -0500

    #25 Initial adminpage, publishing branch

--- erp_frontend/components/adminpage.tsx
+++ erp_frontend/components/adminpage.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Button } from 'evergreen-ui'
-const HomePage = () => {
+
+const AdminPage = () => {
     return (
         <>
             <h1>Welcome to Team's 10 ERP application</h1>
             <p>This is a placeholder paragraph</p>
+            <Button>I am using Evergreen!</Button>
         </>
     )
 }
 
-export default HomePage;
+export default AdminPage;
