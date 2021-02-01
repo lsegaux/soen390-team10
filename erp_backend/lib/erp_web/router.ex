07de5ca09e588ec3694703c22494611537f780c4
lsegaux <laetitia_segaux@hotmail.com>
2021-02-01 02:03:45 -0500

    Authentication

--- erp_backend/lib/erp_web/router.ex
+++ erp_backend/lib/erp_web/router.ex
@@ -1,6 +1,8 @@
 defmodule ErpWeb.Router do
   use ErpWeb, :router
 
+  alias Erp.Guardian
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -13,9 +15,26 @@ defmodule ErpWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :jwt_authenticated do
+    plug Guardian.AuthPipeline
+  end
+
+  scope "/api/v1", ErpWeb do
+    pipe_through [:api, :jwt_authenticated]
+
+    get "/my_user", UserController, :show
+  end
+
   scope "/", ErpWeb do
     pipe_through :browser
 
     get "/*path", PageController, :index
+  end
+
+  scope "/api/v1", ErpWeb do
+    pipe_through :api
+
+    post "/sign_up", UserController, :create
+    post "/sign_in", UserController, :sign_in
   end
 end
