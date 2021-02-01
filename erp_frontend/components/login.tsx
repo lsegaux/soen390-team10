8137e1b346f762bd1d5ec73bf4498a10ae69c8fa
lsegaux <laetitia_segaux@hotmail.com>
2021-02-01 16:09:23 -0500

    Login UI

--- erp_frontend/components/login.tsx
+++ erp_frontend/components/login.tsx
@@ -1,24 +1,25 @@
 import React, { useState, Component } from "react";
 import { Button, FormField, TextInput, Pane } from 'evergreen-ui'
-import axios, { AxiosRequestConfig, AxiosPromise, AxiosResponse } from 'axios';
+import axios from 'axios';
+import { Redirect } from "react-router-dom";
 
 export default function Login() {
   
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [loginError, setLoginError] = useState(false);
   const [password, setPassword] = useState("");
 
   function validateForm() {
-    return username.length > 0 && password.length > 0;
+    return email.length > 0 && password.length > 0;
   }
 
   const handleSubmit = (event: { preventDefault: () => void; }) => {
     event.preventDefault();
-
     const loginData = {
-      email: username,
-      password
+      email: email,
+      password: password
     };
-    
+
         axios({
         method: 'post',
         url: 'http://localhost:4000/api/v1/sign_in',
@@ -26,10 +27,13 @@ export default function Login() {
         data: loginData
         }).then(res => { 
           if (res.status === 200) {
-            console.log(res)
-          } 
+            console.log(res);
+            localStorage.setItem("jwt", res.data.jwt)
+            window.location.href='http://localhost:4000'
+          }
         }).catch(err => { 
-          console.error(err);
+            setLoginError(true);
+            console.error(err);
         });
   }
 
@@ -49,10 +53,10 @@ export default function Login() {
           <FormField label = 'Login' textAlign='center'>
               <div style={{paddingTop: '20px'}}>
                 <TextInput
-                  name="Username"
-                  placeholder="Username"
-                  value={username}
-                  onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setUsername(e.target.value)}
+                  name="Email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setEmail(e.target.value)}
                 />
               </div>
               <div style={{padding: '20px', paddingBottom: '40px'}}>
@@ -65,8 +69,9 @@ export default function Login() {
                 type="password"
                 onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setPassword(e.target.value)}
               />
+                {loginError &&  <FormField validationMessage='Username or password is incorrect' />}
             </div>
-            <Button disabled={!validateForm()} type='submit'  onClick={handleSubmit} >Login</Button>
+            <Button disabled={!validateForm()} type='submit'  onClick={handleSubmit}>Login</Button>
           </FormField>
       </Pane>
     </div>
