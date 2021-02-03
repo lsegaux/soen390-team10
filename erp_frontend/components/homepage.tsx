29ed5571fd36c0c684c677e974aa918d24865299
JoeLoiselle <60014665+JoeLoiselle@users.noreply.github.com>
2021-02-02 23:26:14 -0500

    Added a link to view the Client landing page prototype

--- erp_frontend/components/homepage.tsx
+++ erp_frontend/components/homepage.tsx
@@ -13,6 +13,9 @@ const HomePage = () => {
                 <Link to={'/dashboard'}>
                 <Button>Click this button to be taken to the Dashboard</Button>
                 </Link>
+                <Link to={'/client'}>
+                <Button>Click this button to be taken to the Client Dashboard</Button>
+                </Link>
             </div>
         </>
     )
