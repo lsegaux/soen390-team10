1d202e77e1ad0fc269e1cf7006c6efa5ec2423c4
mounceph99 <mounceph99@hotmail.com>
2021-02-23 08:25:54 -0800

    added recaptcha verification to signup for bot checking

--- erp_frontend/components/signup.tsx
+++ erp_frontend/components/signup.tsx
@@ -3,6 +3,8 @@ import { Button, FormField, TextInput, Pane, Heading, Select } from 'evergreen-u
 import {signUpPost} from "../utils/datafetcher"
 import { Auth } from "../Auth";
 
+import Recaptcha from 'react-recaptcha';
+
 export default function Signup(this: any) {
 
   const [email, setEmail] = useState("");
@@ -12,10 +14,18 @@ export default function Signup(this: any) {
   const [signupError, setSignupError] = useState(false);
   const [passwordTouched, setPasswordTouched] = useState(false);
   const [password, setPassword] = useState("");
+  const [recaptchaVerify,setRecaptchaVerify] = useState(false);
+
+  function verifyCallback(response){
+    if (response){
+      setRecaptchaVerify(true);
+      console.log("hello?")
+    }
+  }
   
   function validateForm() {
     return email.length > 0 && password.length > 8 && firstName.length > 0 &&
-    lastName.length > 0 && userRole.length > 0;
+    lastName.length > 0 && userRole.length > 0 && recaptchaVerify;
   }
 
   const handleSubmit = (event: { preventDefault: () => void; }) => {
@@ -92,7 +102,7 @@ export default function Signup(this: any) {
                     />
                     {passwordTouched && password.length < 8 &&  <FormField label='' validationMessage='Password should be more than 8 characters.' />}
                     {signupError &&  <FormField label='' validationMessage='Email already taken.' />}
-                <Pane style={{padding: '20px', paddingBottom: '40px'}}>
+                <Pane style={{padding: '20px', paddingBottom: '10px'}}>
                     <Select
                       onChange={(e: { target: { value: any; }; }) => setUserRole(e.target.value)}
                       width='100%'
@@ -102,6 +112,16 @@ export default function Signup(this: any) {
                       <option value="Employee">Employee</option>
                     </Select>
                   </Pane>
+
+                    <div style = {{margin:"10px"}}>
+                    <Recaptcha
+                    sitekey="6LcjqWQaAAAAAA_KNpNiiQhQT7rHtpo_AJySY_64"
+                    render="explicit"
+                    onloadCallback={()=>console.log("Im loaded")}
+                    verifyCallback={verifyCallback}
+                    />
+                    </div>
+
                 <Button disabled={!validateForm()} type='submit' appearance="primary" onClick={handleSubmit}>Sign Up</Button>
                 <Button type='submit' marginLeft={15} onClick={ () => {window.location.href = "/login"}}>Go to Login</Button>
                 <Pane marginBottom={15}></Pane>
