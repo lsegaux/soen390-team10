2337ddb87e85878bf084d2d55fce8b5645c0c81c
Nicolas MacBeth <nicolasmacbeth@gmail.com>
2021-02-23 23:36:12 -0500

    db tables and seed data

---
+++ erp_backend/priv/repo/migrations/20210224035415_create_orders.exs
@@ -0,0 +1,13 @@
+defmodule Erp.Repo.Migrations.CreateOrders do
+  use Ecto.Migration
+
+  def change do
+    create table(:orders) do
+      add :orderId, :id
+      add :price, :integer
+      add :userEmail, references(:users, column: :email, type: :string, on_delete: :nothing)
+      add :time, :naive_datetime
+      add :bikesAmount, :integer
+    end
+  end
+end
