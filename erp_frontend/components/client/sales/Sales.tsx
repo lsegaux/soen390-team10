0d1280109432ffcfec7c1c3220c828cd255bec74
lsegaux <laetitia_segaux@hotmail.com>
2021-02-24 10:55:46 -0500

    First commit Sales connect frontend and backend

--- erp_frontend/components/client/sales/Sales.tsx
+++ erp_frontend/components/client/sales/Sales.tsx
@@ -20,6 +20,8 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
+import { RadioGroup } from '@material-ui/core';
+import { Label } from 'recharts';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -53,53 +55,159 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 );
 
-export default function Sales() {
 
-    let wheels = {
-        type: 'Wheels',
-        material: ['carbon', 'aluminum']
+export default function Sales() {
+    
+    var parts = {
+        "Wheels": [
+        ],
+        "Frame": [
+        ],
+        "Handlebars": [
+        ],
+        "Brakes": [
+        ],
+        "Tires": [
+        ],
+        "Seats": [
+        ],
+        "Fork": [
+        ],
+        "Pedals": [
+        ]
+    };
+
+    function parseInventory(){
+        // TODO: query backend to get real info
+        var plants = [
+            {
+              plantName: "Montreal",
+              parts: [
+                 {
+                  type: "Wheels",
+                  material: "Carbon",
+                  price: "200$",
+                  quantity: 3,
+                  }
+              ]
+            },
+            {
+                plantName: "Toronto",
+                parts: [
+                   {
+                    type: "Wheels",
+                    material: "Aluminium",
+                    price: "22$",
+                    "quantity": 230,
+                    },
+                    {
+                    type: "Wheels",
+                    material: "Carbon",
+                    price: "200$",
+                    quantity: 10,
+                    }
+                ]
+              }
+          ]
+
+          for (var i=0; i <plants.length; i++){
+              for (var j = 0; j < plants[i]["parts"].length; j++){
+
+                  const partName = plants[i]["parts"][j]["type"];
+                  const materialName = plants[i]["parts"][j]["material"];  
+                  const quantity = plants[i]["parts"][j]["quantity"]
+                  const price = plants[i]["parts"][j]["price"]
+                  
+                  if (!(materialName in parts[partName])){
+                      parts[partName][materialName] = {price: price, quantity:quantity}
+                  }
+                  else {
+                    let currQuantity = parts[partName][materialName]["quantity"];
+                    parts[partName][materialName]["quantity"] = quantity + currQuantity
+                  }
+              }
+              console.log(parts);
+          }
     }
 
+    parseInventory()
+    let wheels = {
+        partName: "Wheels",
+        material: ['Carbon', 'Aluminium'],
+        types: parts["Wheels"]
+    };
+
+    let frames = {
+        partName: "Frame",
+        material: ['Carbon', 'Aluminum', 'Alloy', 'Steel'],
+        types: parts["Frame"]
+    };
+
     let handlebars = {
-        type: 'Handlebars',
-        material: ['carbon', 'aluminum', 'alloy', 'steel']
+        partName: 'Handlebars',
+        material: ['Carbon', 'Aluminium', 'Alloy', 'Steel'],
+        types: parts["Hadndlebars"]
     }
 
     let brakes = {
-        type: 'Handlebar',
-        material: ['carbon', 'aluminum', 'alloy', 'steel']
+        partName: 'Brakes',
+        material: ['Carbon', 'Aluminium', 'Alloy', 'Steel'],
+        types: parts["Brakes"]
     }
 
     let tires = {
-        type: 'Tires',
-        material: ['Summer', 'Winter']
+        partName: 'Tires',
+        material: ['Summer', 'Winter'],
+        types: parts["Tires"]
     }
 
     let seats = {
-        type: 'Seats',
-        material: ['Cloth', 'Leather']
-    }
-
-    let frames = {
-        type: 'Frames',
-        material: ['carbon', 'aluminum', 'alloy', 'steel']
+        partName: 'Seats',
+        material: ['Cloth', 'Leather'],
+        types: parts["Seats"]
     }
 
     let forks = {
-        type: 'Forks',
-        material: ['carbon', 'aluminum', 'alloy', 'steel']
+        partName: 'Forks',
+        material: ['Carbon', 'Aluminium', 'Alloy', 'Steel'],
+        types: parts["Forks"]
     }
 
     let pedals = {
-        type: 'Pedals',
-        material: ['plastic road bike petals', 'aluminum road bike petals', 'plastic mountain bike petals', 'aluminum mountain bike petals']
+        partName: 'Pedals',
+        material: ['Plastic road bike petals', 'Aluminum road bike petals', 'Plastic mountain bike petals', 'Aluminum mountain bike petals'],
+        types: parts["Pedals"]
     }
-
 
     let bikeParts = [wheels, handlebars, brakes, tires, seats, frames, forks, pedals];
     const [bikePartsArr, setBikePartsArr] = useState(bikeParts);
+    const [wheelMaterial, setWheelMaterial] = useState("");
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [bikeQty, setBikeQty] = useState(0);
 
     const classes = useStyles();
+
+    function handleBikeQty(value: string){
+        let numBikes = parseInt(value);
+        if (numBikes < 15){
+            alert("You must order at least 15 bikes");
+            return;
+        }
+        setBikeQty(numBikes)
+    }
+
+    function wheelButtonChange(value: string){
+        let qty = wheels["types"][value]["quantity"]
+        console.log(qty)
+        console.log(bikeQty)
+        if (qty < bikeQty){
+            alert(`This part is out of stock (only ${qty} left).`);
+            return;
+        }
+        setWheelMaterial(value);
+        let price = wheels["types"][value]["price"].substring(0,wheels["types"][value]["price"].length - 1);
+        setTotalPrice(parseInt(totalPrice + price));
+    }
 
     return (
         <div className={classes.sales}>
@@ -123,6 +231,7 @@ export default function Sales() {
                             <Paper className={classes.paperSales}>
                                 <Title>Build a bike</Title>
                                 <br></br>
+                                <TextField id="filled-basic" label="Enter quantity of bikes" variant="filled" onMouseLeave={(e: { target: { value: React.SetStateAction<string>; }; }) => handleBikeQty(e.target.value)}/>
                                 <Typography>Please select type of material for each category.</Typography>
                                 <br></br>
                                 <Table size="small">
@@ -134,11 +243,13 @@ export default function Sales() {
                                     </TableHead>
                                     <TableBody>
                                         {bikePartsArr.map((bike) => (
-                                            <TableRow key={bike.type}>
-                                                <TableCell>{bike.type}</TableCell>
+                                            <TableRow key={bike.partName}>
+                                                <TableCell>{bike.partName}</TableCell>
                                                 {bike.material.map((material) => (
-                                                    <TableCell key={bike.type + material}>
-                                                        <FormControlLabel value="end" control={<Radio color="primary" />} label={material} />
+                                                    <TableCell key={bike.types + material}>
+                                                        <RadioGroup onChange={(e: { target: { value: React.SetStateAction<String>; }; }) => wheelButtonChange(e.target.value)}>
+                                                        <FormControlLabel value={material} control={<Radio color="primary" />} label={material} />
+                                                        </RadioGroup>
                                                     </TableCell>
                                                 ))}
                                             </TableRow>
@@ -150,13 +261,13 @@ export default function Sales() {
                                     <Button variant="contained" color="primary" href="#contained-buttons">
                                         Order
                                     </Button>
-                                    <TextField id="filled-basic" label="Enter quantity of bikes" variant="filled" />
                                 </Box>
+                                <Title>Total: {totalPrice} </Title>
                             </Paper>
                         </Grid>
                     </Grid>
                 </Container>
             </main>
-        </div >
+        </div>
     );
 }
