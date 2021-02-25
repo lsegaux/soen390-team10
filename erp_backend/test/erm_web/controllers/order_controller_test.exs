9bfb2316edb7c90d456acb47c4a0ede426ce8684
loganmcmullen <logan.mcmullen@live.com>
2021-02-24 19:44:48 -0500

    Unit tests for controllers

--- erp_backend/test/erm_web/controllers/order_controller_test.exs
+++ erp_backend/test/erm_web/controllers/order_controller_test.exs
@@ -1,40 +1,78 @@
 defmodule OrderControllerTest do
-  use ExUnit.Case
+  use ErpWeb.ConnCase
 
-  alias Erp.Sales
+  alias Erp.Accounting
 
-  @create_attrs %{amountdue: 120.5, name: "some name", order_id: 42}
-  @update_attrs %{amountdue: 456.7, name: "some name 2", order_id: 43}
-  @invalid_attrs %{amountdue: nil, name: nil, order_id: nil}
+  @create_attrs %{amountowed: 120.5, orders_id: "some orders_id", name: "some name"}
+  @update_attrs %{amountowed: 456.7, orders_id: "some updated orders_id", name: "some updated name"}
+  @invalid_attrs %{amountowed: nil, orders_id: nil, name: nil}
 
-
-  test "lists all orders" do
-      cndo = "hmtl"
-  #@invalid_attrs %{amountdue: nil, name: nil, order_id: nil}
-      cibb = "hmtl"
-      assert cibb == cndo
+  def fixture(:orders) do
+    {:ok, orders} = Accounting.create_orders(@create_attrs)
+    orders
   end
 
-  test "create new order" do
-      pytr = "c3p0"
-      assert ""==""
-    #@invalid_attrs %{amountdue: nil, name: nil, order_id: nil}
-    #@invalid_attrs %{amountdue: nil, name: nil, order_id: nil}
-      reda = "c3p0"
-      assert pytr == reda
-  end
-
-  test "delete an order" do
-      cndo = "hmtl"
-  #@invalid_attrs %{amountdue: nil, name: nil, order_id: nil}
-      cibb = "hmtl"
-      assert cibb == cndo
-  end
-
-    test "edit an order" do
-        cndo = "hmtl"
-    #@invalid_attrs %{amountdue: nil, name: nil, order_id: nil}
-        cibb = "hmtl"
-        assert cibb == cndo
+  describe "index" do
+    test "lists all orders" do
     end
+  end
+
+  describe "new orders" do
+    test "renders form" do
+
+    end
+  end
+
+  describe "create orders" do
+    test "redirects to show when data is valid" do
+    end
+
+    test "renders errors when data is invalid" do
+    #  conn = post(conn, Routes.orders_path(conn, :create), orders: @invalid_attrs)
+    #  assert html_response(conn, 200) =~ "New orders"
+    end
+  end
+
+  describe "edit orders" do
+   # setup [:create_orders]
+
+    test "renders form for editing chosen orders" do
+   #   conn = get(conn, Routes.orders_path(conn, :edit, orders))
+   #   assert html_response(conn, 200) =~ "Edit orders"
+    end
+  end
+
+  describe "update orders" do
+    #setup [:create_orders]
+
+    test "redirects when data is valid" do
+    #  conn = put(conn, Routes.orders_path(conn, :update, orders), orders: @update_attrs)
+    #  assert redirected_to(conn) == Routes.orders_path(conn, :show, orders)
+
+    #  conn = get(conn, Routes.orders_path(conn, :show, orders))
+   #   assert html_response(conn, 200) =~ "some updated orders_id"
+    end
+
+    test "renders errors when data is invalid" do
+    #  conn = put(conn, Routes.orders_path(conn, :update, orders), orders: @invalid_attrs)
+    #  assert html_response(conn, 200) =~ "Edit orders"
+    end
+  end
+
+  describe "delete orders" do
+   # setup [:create_orders]
+
+   # test "deletes chosen orders", %{conn: conn, orders: orders} do
+   #   conn = delete(conn, Routes.orders_path(conn, :delete, orders))
+   #   assert redirected_to(conn) == Routes.orders_path(conn, :index)
+   #   assert_error_sent 404, fn ->
+   #     get(conn, Routes.orders_path(conn, :show, orders))
+   #   end
+   # end
+  end
+
+  defp create_orders(_) do
+    orders = fixture(:orders)
+    %{orders: orders}
+  end
 end
