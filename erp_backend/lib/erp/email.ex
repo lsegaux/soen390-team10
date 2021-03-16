1bb3ebc116027bcf0f46a0b91f8717c07d5d4f39
Greg <60011793+gregtuc@users.noreply.github.com>
2021-03-16 12:56:11 -0400

    Shipping Status Updates #58 #56 #57
    Functionality for three client-side email updates:
    
    1) Order Confirmation.
    2) Shipping in Progress.
    3) Shipping Complete.

--- erp_backend/lib/erp/email.ex
+++ erp_backend/lib/erp/email.ex
@@ -1,11 +1,27 @@
 defmodule Erp.Email do
     use Bamboo.Phoenix, view: ErpWeb.EmailView
 
-    def welcome_text_email(email_address) do
+    def order_confirmation_email(email_address, id, bikesAmount, price, time) do
         new_email
         |> to(email_address)
         |> from("390teamten@gmail.com")
-        |> subject("Welcome!")
-        |> text_body("Welcome to Adrenaline ERP")
+        |> subject("Order Confirmation")
+        |> render("order_confirmation_email.html", email_address: email_address, id: id, bikesAmount: bikesAmount, price: price, time: time)
+    end
+
+    def order_shipped_email(email_address, id, delivery_date) do
+        new_email
+        |> to(email_address)
+        |> from("390teamten@gmail.com")
+        |> subject("Order has been shipped.")
+        |> render("order_shipped_email.html", email_address: email_address, id: id, delivery_date: delivery_date)
+    end
+
+    def order_delivered_email(email_address, id, delivery_date) do
+        new_email
+        |> to(email_address)
+        |> from("390teamten@gmail.com")
+        |> subject("Order has been delivered.")
+        |> render("order_delivered_email.html", email_address: email_address, id: id, delivery_date: delivery_date)
     end
 end
--- erp_backend/lib/erp_web/controllers/email_controller.ex
+++ erp_backend/lib/erp_web/controllers/email_controller.ex
@@ -3,8 +3,18 @@ defmodule ErpWeb.EmailController do
 
     alias Erp.Repo
 
-    def send_email(conn, %{"email" => email_address}) do
-        Erp.Email.welcome_text_email(email_address) |> Erp.Mailer.deliver_now
+    def send_order_confirmation_email(conn, %{"email" => email_address, "id" => id, "bikesAmount" => bikesAmount, "price" => price, "time" => time}) do
+        Erp.Email.order_confirmation_email(email_address, id, bikesAmount, price, time) |> Erp.Mailer.deliver_now
+        render(conn, "index.json", email_address: email_address)
+    end
+
+    def send_order_shipped_email(conn, %{"email" => email_address, "id" => id}) do
+        Erp.Email.order_shipped_email(email_address, id, DateTime.utc_now |> DateTime.add(5*24*60*60, :second)) |> Erp.Mailer.deliver_now
+        render(conn, "index.json", email_address: email_address)
+    end
+
+    def send_order_delivered_email(conn, %{"email" => email_address, "id" => id}) do
+        Erp.Email.order_delivered_email(email_address, id, DateTime.utc_now) |> Erp.Mailer.deliver_now
         render(conn, "index.json", email_address: email_address)
     end
 end
