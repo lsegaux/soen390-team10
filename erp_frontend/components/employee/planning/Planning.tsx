b32ff988c9557af6501bc453b641a6c724595ecb
Dina Khalesi <dinakhalesit@gmail.com>
2021-04-01 03:23:54 -0400

    testing gantt chart

--- erp_frontend/components/employee/planning/Planning.tsx
+++ erp_frontend/components/employee/planning/Planning.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -13,6 +13,13 @@ import FolderIcon from '@material-ui/icons/Folder';
 import DeleteIcon from '@material-ui/icons/Delete';
 import AddIcon from '@material-ui/icons/Add';
 import EditIcon from '@material-ui/icons/Edit';
+import Button from '@material-ui/core/Button';
+import { RadioGroup, Dialog, DialogContent, DialogContentText, DialogTitle, DialogActions } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 //@ts-ignore
 import TimeLine from "react-gantt-timeline";
 
@@ -42,6 +49,13 @@ const useStyles = makeStyles((theme) => ({
       fontFamily: 'sans-serif',
       fontStyle: 'normal',
     },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+      },
+    selectEmpty: {
+    marginTop: theme.spacing(2),
+    },
 
 }));
 
@@ -56,13 +70,65 @@ function generate(element:any) {
 
 export default function Planning(){
     const classes = useStyles();
-    const [dense, setDense] = React.useState(false);
-    const [secondary, setSecondary] = React.useState(false);
-    let data=[ {id:1,start:new Date(), end:Date.now()+1 ,name:'Task 1'},
-            {id:2,start:new Date(), end:Date.now()+1 ,name:'Task 2'}, {id:2,start:new Date(), end:Date.now()+1 ,name:'Task 3'}]
+    const [dense, setDense] = useState(false);
+    const [secondary, setSecondary] = useState(false);
+    const [openTaskForm, setOpenTaskForm] = useState(false);
+    const [year, setYear] = useState(0);
+    const [month, setMonth] = useState(0);
+    const [day, setDay] = useState(0);
+    const [hour, setHour] = useState(0);
+    const [minute, setMinute] = useState(0);
+    const [AMPM, setAMPM] = useState("AM");
+    const [taskName, setTaskName] = useState("");
+    const [taskDescription, setTaskDescription] = useState("");
+    const [age, setAge] = React.useState('');
+    const [value, setValue] = React.useState(null);
 
-    let links=[ {id:1,start:1, end:2},
-                {id:2,start:1, end:3}]
+    const handleDate = (value) => {
+        console.log("hey", value)
+    }
+
+    const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        setAge(event.target.value as string);
+    };
+
+    const handleOpen = () => {
+        setOpenTaskForm(true);
+    }
+
+    const handleClose = () => {
+        setOpenTaskForm(false);
+    }
+
+    const handleSave = () => {
+        console.log("saved");
+    }
+
+    
+
+    let d1 = new Date();
+    console.log(d1)
+    let d2 = new Date();
+    d2.setDate(d2.getDate() + 5);
+    let d3 = new Date();
+    d3.setDate(d3.getDate() + 8);
+    let d4 = new Date();
+    d4.setDate(d4.getDate() + 20);
+    let data = [
+      {
+        id: 1,
+        start: d1,
+        end: d2,
+        name: "Demo Task 1"
+      },
+      {
+        id: 2,
+        start: d3,
+        end: d4,
+        name: "Demo Task 2",
+        color: "orange"
+      }
+    ];
   return (
     <>
     <style>
@@ -74,9 +140,55 @@ export default function Planning(){
                 <Typography variant="h6" className={classes.title}>
                     <br/>
                 &nbsp;Tasks
-                    <IconButton>
+                    <IconButton onClick={handleOpen}>
                     <AddIcon />
                     </IconButton>
+                    <Dialog open={openTaskForm} onClose={handleClose} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Task Details</DialogTitle>
+                        <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Task Name"
+                            type="string"
+                            fullWidth
+                            onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setTaskName(e.target.value)}
+                        />
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                            <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={age}
+                            onChange={handleChange}
+                            >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Description"
+                            type="string"
+                            fullWidth
+                            onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setTaskDescription(e.target.value)}
+                        />
+                        <label for="start">Start date:</label>
+                        <input type="date" id="start" name="trip-start"
+                            value="2018-07-22"
+                            min="2018-01-01" max="2018-12-31"
+                            onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => handleDate(e.target.value)}></input>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleSave} color="primary">
+                                Save Task
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
                 </Typography>
                 <div className={classes.demo}>
                     <List dense={dense}>
@@ -107,18 +219,12 @@ export default function Planning(){
             </div>
             <div className = {classes.title}>
                 <Typography variant="h6" className={classes.title}>
-                &nbsp;Schedule New Meeting
-                   <IconButton>
-                    <AddIcon />
-                 </IconButton>
-                </Typography>
-                <Typography variant="h6" className={classes.title}>
                 &nbsp;Gantt Chart
                 </Typography>
             </div>
 
             <div>
-            <TimeLine data={data} links={links} />
+            <TimeLine data={data} />
                 <br/>
                 <br/>
             </div>
